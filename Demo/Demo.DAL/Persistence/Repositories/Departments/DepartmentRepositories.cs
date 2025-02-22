f6f8bc102db671150d2a97a8bdd275278d056184
farahmohmed <102584837+farah2543@users.noreply.github.com>
2025-02-22 22:01:11 +0200

    4- DAL -Department Repository

---
+++ Demo/Demo.DAL/Persistence/Repositories/Departments/DepartmentRepositories.cs
@@ -0,0 +1,58 @@
+﻿using Demo.DAL.Entities.Departments;
+using Demo.DAL.Persistence.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Persistence.Repositories.Departments
+{
+    internal class DepartmentRepositories : IDepartmentRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public DepartmentRepositories(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            
+        }
+        public ICollection<Department> GetAll(bool asNoTracking = true)
+        {
+            if (asNoTracking)
+            {
+
+                _dbContext.Departments.AsNoTracking().ToList();
+
+            }
+            return _dbContext.Departments.ToList();
+        }
+
+        public Department? GetById(int departmentId)
+        {
+            //return _dbContext.Departments.Local.FirstOrDefault(d => d.Id == departmentId);
+            return _dbContext.Departments.Find(departmentId); // search locally first 
+        }
+        public int AddDepartment(Department Entity)
+        {
+            _dbContext.Departments.Add(Entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public int UpdateDepartment(Department Entity)
+        {
+            _dbContext.Departments.Update(Entity);
+            return _dbContext.SaveChanges();
+        }
+        public int DeleteDepartment(Department Entity)
+        {
+            _dbContext.Departments.Remove(Entity);
+            return _dbContext.SaveChanges();
+           
+        }
+
+   
+
+     
+    }
+}
---
+++ Demo/Demo.DAL/Persistence/Repositories/Departments/IDepartmentRepository.cs
@@ -0,0 +1,29 @@
+﻿using Demo.DAL.Entities.Departments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Persistence.Repositories.Departments
+{
+    public interface IDepartmentRepository
+    {
+         ICollection<Department> GetAll(bool asNoTracking = true);
+        Department? GetById(int departmentId);
+        int AddDepartment (Department Entity);
+
+        int UpdateDepartment(Department Entity);
+
+
+        int DeleteDepartment(Department Entity);
+
+
+
+
+
+
+
+
+    }
+}
