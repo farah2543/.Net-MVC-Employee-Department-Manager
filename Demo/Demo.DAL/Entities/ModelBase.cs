6975f7f42a589f7bd53c633b6afea4a1719c4629
farahmohmed <102584837+farah2543@users.noreply.github.com>
2025-02-22 19:47:18 +0200

    2-DAL

---
+++ Demo/Demo.DAL/Entities/ModelBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Entities
+{
+    internal class ModelBase
+    {
+        public int Id { get; set; }
+
+        public int CreatedBy { get; set; }
+        public DateTime CreateOn { get; set; }
+        public int LastModifiedBy { get; set; }
+        public DateTime LastModifiedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+
+    }
+}
---
+++ Demo/Demo.DAL/Persistence/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Persistence.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() :base()
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=MVCProject;Trusted_Connection=true;TrustServerCertificate=true");
+        }
+
+
+    }
+}
