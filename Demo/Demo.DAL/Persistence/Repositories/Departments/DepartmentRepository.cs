c2162eea07bd651198312ae02f7cafb6daff66a5
farahmohmed <102584837+farah2543@users.noreply.github.com>
2025-03-05 00:50:20 +0200

    2- Employee Repository

--- Demo/Demo.DAL/Persistence/Repositories/Departments/DepartmentRepository.cs
+++ Demo/Demo.DAL/Persistence/Repositories/Departments/DepartmentRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Demo.DAL.Entities.Departments;
 using Demo.DAL.Persistence.Data;
+using Demo.DAL.Persistence.Repositories.Generic;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -9,56 +10,12 @@ using System.Threading.Tasks;
 
 namespace Demo.DAL.Persistence.Repositories.Departments
 {
-    public class DepartmentRepository : IDepartmentRepository
+    public class DepartmentRepository : GenericRepository<Department>, IDepartmentRepository
     {
-        private readonly ApplicationDbContext _dbContext;
-        public DepartmentRepository(ApplicationDbContext dbContext)
+        public DepartmentRepository(ApplicationDbContext applicationDbContext): base(applicationDbContext)
         {
-            _dbContext = dbContext;
             
         }
-        public ICollection<Department> GetAll(bool asNoTracking = true)
-        {
-            if (asNoTracking)
-            {
 
-                _dbContext.Departments.AsNoTracking().ToList();
-
-            }
-            return _dbContext.Departments.ToList();
-        }
-
-        public Department? GetById(int departmentId)
-        {
-            //return _dbContext.Departments.Local.FirstOrDefault(d => d.Id == departmentId);
-            return _dbContext.Departments.Find(departmentId); // search locally first 
-        }
-        public int AddDepartment(Department Entity)
-        {
-            _dbContext.Departments.Add(Entity);
-            return _dbContext.SaveChanges();
-        }
-
-        public int UpdateDepartment(Department Entity)
-        {
-            _dbContext.Departments.Update(Entity);
-            return _dbContext.SaveChanges();
-        }
-        public int DeleteDepartment(Department Entity)
-        {
-            _dbContext.Departments.Remove(Entity);
-            return _dbContext.SaveChanges();
-           
-        }
-
-        IEnumerable<Department> IDepartmentRepository.GetAll(bool asNoTracking)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IQueryable<Department> GetAllQuerable()
-        {
-            return _dbContext.Departments;
-        }
     }
 }
