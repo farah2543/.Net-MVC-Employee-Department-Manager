a1c974b7403d7cf0cc91cefa62689798235cc598
farahmohmed <102584837+farah2543@users.noreply.github.com>
2025-03-06 03:36:35 +0200

    7- Empolyee Controller -Delete

--- Demo/Demo.BLL/DTOs/Empolyees/EmployeeToReturnDto.cs
+++ Demo/Demo.BLL/DTOs/Empolyees/EmployeeToReturnDto.cs
@@ -1,6 +1,7 @@
 ﻿using Demo.DAL.Entities.Common.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,17 @@ namespace Demo.BLL.DTOs.Employees
         public int Id { get; set; }
         public string Name { get; set; } = null!;
         public int? Age { get; set; }
+
+        [DataType(DataType.Currency)]
         public decimal salary { get; set; }
+        [EmailAddress]
         public string? Email { get; set; }
+
+        [Display(Name = "IS Active")]
         public bool ISActive { get; set; }
         public string Gender { get; set; }
+
+        [Display(Name ="Employee Type")]
         public string EmployeeType { get; set; }
     }
 }
