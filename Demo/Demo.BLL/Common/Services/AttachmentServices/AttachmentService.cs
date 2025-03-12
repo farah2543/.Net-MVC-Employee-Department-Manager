ac919de6a882c73f81304be568082dd46633e7df
farahmohmed <102584837+farah2543@users.noreply.github.com>
2025-03-12 17:54:17 +0200

    5- Attachment Service

---
+++ Demo/Demo.BLL/Common/Services/AttachmentServices/AttachmentService.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.BLL.Common.Services.AttachmentServices
+{
+    public class AttachmentService : IAttachmentService
+    {
+        public readonly List<string> _allowedExtensions = new() { ".png" , ".jpg" , "jpeg"};
+
+        public const int _maxAllowedSize = 2_097_152;
+
+        public string? Upload(IFormFile file, string folderName)
+        {
+            var extension = Path.GetExtension(file.FileName);
+
+            if (!_allowedExtensions.Contains(extension))
+            {
+                return null;
+            }
+            if(file.Length > _maxAllowedSize)
+            {
+                return null;
+            }
+            var folderPath = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot\\files",folderName);
+
+            var fileName = $"{Guid.NewGuid()}{extension}";
+
+            var filePath = Path.Combine(folderPath,fileName);
+
+            using var fileStream = new FileStream(filePath,FileMode.Create);
+
+            file.CopyTo(fileStream);
+
+            return fileName;
+
+        }
+        public bool Delete(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+                return true;
+            }
+            return false;
+        }
+
+       
+    }
+}
---
+++ Demo/Demo.BLL/Common/Services/AttachmentServices/IAttachmentService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.BLL.Common.Services.AttachmentServices
+{
+    public interface IAttachmentService
+    {
+        public string? Upload(IFormFile file, string folderName);
+
+        public bool Delete(string FilePath);
+
+
+
+    }
+}
