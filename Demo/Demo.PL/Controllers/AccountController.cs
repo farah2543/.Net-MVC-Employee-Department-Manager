2aa91b8da09d0e72af5d377ba2e5b917c080f80f
farahmohmed <102584837+farah2543@users.noreply.github.com>
2025-03-15 21:30:09 +0200

    4- Account Controller

---
+++ Demo/Demo.PL/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.PL.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+
+}
