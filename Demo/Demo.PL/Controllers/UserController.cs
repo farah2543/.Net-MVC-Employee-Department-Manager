113a8b424a466ad6f2bf4847ae071bc98810012f
farahmohmed <102584837+farah2543@users.noreply.github.com>
2025-03-20 12:12:20 +0200

    4- User Controller

---
+++ Demo/Demo.PL/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using Demo.DAL.Entities.Identity;
+using Demo.PL.ViewModels.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.PL.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index(string SearchValue)
+        {
+            // Users
+            var usersQuery = _userManager.Users.AsQueryable();
+            if (!string.IsNullOrEmpty(SearchValue)) // SearchValue = mariam
+            {
+                usersQuery = usersQuery.Where(U => U.Email.ToLower().Contains(SearchValue.ToLower()));
+            }
+
+            var usersList = await usersQuery.Select(U => new UserViewModel
+            {
+                Id = U.Id,
+                FName = U.FName,
+                LName = U.LName,
+                Email = U.Email
+            }).ToListAsync();
+
+            foreach (var user in usersList)
+            {
+                user.Roles = await _userManager.GetRolesAsync(await _userManager.FindByIdAsync(user.Id));
+            }
+
+            return View(usersList);
+        }
+
+
+    }
+}
